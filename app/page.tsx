442e24c2df364943695f18a32119a9887d8f5c2f
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-11-22 20:52:54 +0500

    cw

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-import link from "next/link";
-import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+
+
+
+
+
+
 
 export default function Home() {
-  return (
-    <>
-    <div className="container mx-auto p-4 pt-6 md:p-6 lg:p
-    </>
-  )
-};
+  const router = useRouter(); 
+
+  const moveToConatct = () => {
+    router.push("/contact");
+  };
+
+  useEffect(())
+} 

fefe76ffd3ada956f658dccf3fafe9a5b78ce942
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-11-22 20:20:52 +0500

    clas work

--- app/page.tsx
+++ app/page.tsx
@@ -2,4 +2,8 @@ import Image from "next/image";
 import link from "next/link";
 import { useRouter } from "next/router";
 
-export default
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen
+  )
+}
